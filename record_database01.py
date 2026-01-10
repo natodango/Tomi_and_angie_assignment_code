b21187e5899552a67637f58e383ce0ebcd06ff0f
Nathaniel Godwin <168899179+natodango@users.noreply.github.com>
2026-01-10 22:52:56 +0100

    Create record_database01.py

---
+++ record_database01.py
@@ -0,0 +1,15 @@
+full_name = input("What is your fullname?: ")
+country = input("Which country do you live in?: ")
+status = input("what is your citizenship status (Active,Pending,Close)?: ")
+state = input("which state do you reside?: ")
+religion = input("what your religion?: ")
+
+
+print(f"""
+-------------- Citizen👨Database📔Record -------------
+fullname: {full_name}
+country: {country}
+status: {status}
+state: {state}
+religion: {religion}
+""")
